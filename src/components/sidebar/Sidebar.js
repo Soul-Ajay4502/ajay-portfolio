91b42357ed9eab356e6e112707bced1d06fce5c5
Ajay <ajay.raj@roadelabs.com>
2024-10-08 12:47:10 +0530

    sidebar updated for light theme

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -6,63 +6,71 @@ import { ReactComponent as Email } from '../../assets/icon/SolidEmail.svg';
 import DropDown from './Dropdown';
 import PdfDownload from '../pdfDownloader';
 import { useTheme } from '../../context/ThemeContext';
-import { justifyContent } from 'styled-system';
+import { FaRegCircleLeft } from "react-icons/fa6";
+import { borderBottom, shadow } from 'styled-system';
 
 function Sidebar({ show, closeSidebar }) {
     const navigate = useNavigate();
     const [openedMenu, setOpenedMenu] = useState(null);
-    const { contextCurrentSection } = useTheme();
+    const { contextCurrentSection, themeStyles, theme } = useTheme();
 
-    const toggleMenu = (item) => {
-        if (openedMenu === item) {
-            setOpenedMenu(null);
-            return;
+    const linkBg = {
+        light: {
+            bg: 'linear-gradient(167deg, #2299C7 18%, #A7CEFF 57%)',
+            shadow: 'rgba(6, 0, 0, 0.5) -1px 8px 9px'
         }
-        setOpenedMenu(item);
-    };
+        ,
+        dark: { bg: '#000', shadow: '' }
+    }
 
     return (
         <Flex
             {...styles.container}
+            bg={themeStyles.backgroundColor}
             style={{
                 transform: show ? 'translateX(0%)' : 'translateX(-100%)',
                 transition: '.15s ease-out',
             }}
         >
-            <Div 
-            mt='150px'
-            position='relative'
+            <Div
+                mt='150px'
+                position='relative'
             >
-                <Flex 
-                position='absolute' 
-                top='-110px' 
-                right='25px'
-                fontSize='43px'
-                onClick={closeSidebar}
-                border='2px solid rgba(255, 255, 255, 0.3)'
-                bg='rgba(140, 86, 36, 0.5)'
-                borderRadius='80px'
-                px='10px'
-                height='46px'
-                justifyContent='center'
-                alignItems='center'
+                <Flex
+                    position='absolute'
+                    top='-110px'
+                    right='0'
+                    fontSize='30px'
+                    onClick={closeSidebar}
+                    borderRadius='80px'
+                    px='10px'
+                    height='46px'
+                    justifyContent='center'
+                    alignItems='center'
                 >
-                &times;
+                    <FaRegCircleLeft />
+                    {/* &times; */}
                 </Flex>
-                <Link smooth to='tel:+919846027693' onClick={() => { closeSidebar(); contextCurrentSection({ section: 'section1' }) }} {...styles.link}>
+                <Link style={{ background: linkBg[theme].bg, borderTopRightRadius: '20px', borderTopLeftRadius: '20px' }} smooth to='tel:+919846027693' onClick={() => { closeSidebar(); contextCurrentSection({ section: 'section1' }) }} {...styles.link}>
                     <Flex alignItems='center'>
                         <Call />
                         <Span pl='3'>+91-9846027693</Span>
                     </Flex>
                 </Link>
 
-                <Link smooth to='mailto:rajay5767@gmail.com' onClick={() => { closeSidebar(); contextCurrentSection({ section: 'section2' }) }} {...styles.link}>
+                <Link style={{ background: linkBg[theme].bg, border: 'none' }} smooth to='mailto:rajay5767@gmail.com' onClick={() => { closeSidebar(); contextCurrentSection({ section: 'section2' }) }} {...styles.link}>
                     <Flex alignItems='center'>
                         <Email />
                         <Span pl='3'>rajay5767@gmail.com</Span>
                     </Flex>
                 </Link>
-                <Link smooth to='https://www.linkedin.com/in/ajayraj-a-r' onClick={() => { closeSidebar(); contextCurrentSection({ section: 'section3' }) }} {...styles.link}>
+                <Link style={{
+                    background: linkBg[theme].bg,
+                    borderBottomRightRadius: '20px',
+                    borderBottomLeftRadius: '20px',
+                    boxShadow: linkBg[theme].shadow,
+                    borderBottom:'none'
+                }} smooth to='https://www.linkedin.com/in/ajayraj-a-r' onClick={() => { closeSidebar(); contextCurrentSection({ section: 'section3' }) }} {...styles.link}>
                     <Flex alignItems='center'>
                         <Flex
                             borderRadius='100px'
@@ -70,7 +78,7 @@ function Sidebar({ show, closeSidebar }) {
                             alignItems='center'
                         ><Div fontWeight='700' fontSize='20px' color='#fff'>In</Div>
                         </Flex>
-                        <Span pl='3'>Ajayraj A R</Span>
+                        <Span pl='3' color='#fff'>Ajayraj A R</Span>
                     </Flex>
                 </Link>
             </Div>
@@ -87,7 +95,7 @@ const styles = {
         minHeight: '100vh',
         height: '100vh',
         flexWrap: 'nowrap',
-        bg: 'dark',
+        // bg: 'dark',
         position: 'fixed',
         zIndex: '10001',
         bottom: '0',
@@ -96,7 +104,7 @@ const styles = {
         backgroundColor: '#15171A',
         fontFamily: 'inter',
         overflowY: 'auto',
-        justifyContent:'space-between'
+        justifyContent: 'space-between'
     },
     link: {
         fontSize: { xs: '20px', md: '24px' },
